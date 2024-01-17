e40b6fdb2838574b8c7404bc4cc421d580180552
Daniel Díaz <diaz_carrete@yahoo.com>
2024-01-17 22:57:23 +0100

    some explanations

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,15 @@ import Type.Reflection qualified
 
 type FrameList a = [a]
 
+-- |
+-- This type remembers that the elements live in a list, but statically "forgets" about
+-- what type they are. However, the 'Typeable' constraint ensures that we can still compare them
+-- with other types at runtime!
+--
+-- The definition uses GADT syntax.
+--
+-- As usual when GADTs are afoot (and Data.Typeable.(:~:) is another GADT) you should expect GHC to
+-- "gain" information about types at runtime while pattern-matching.
 data SomeFrameList where
   MakeSomeFrameList :: (Typeable a) => {frameList :: FrameList a} -> SomeFrameList
 
@@ -24,8 +33,11 @@ data DataFrame = DataFrame
 
 data ApplyError
   = ColumnDoesNotExist ColumnName
-  | -- note: 'SomeTypeRep' from 'Type.Reflection' corresponds to 'TypeRep' from 'Data.Typeable'.
-    -- The 'TypeRep' from 'Type.Reflection' is statically indexed by the type, the one from 'Data.Typeable' doesn't.
+  | -- note: 'SomeTypeRep' from 'Type.Reflection' corresponds to 'TypeRep'
+    -- from 'Data.Typeable' (the latter is a type synonym for the former).
+    --
+    -- The 'TypeRep' from 'Type.Reflection' is statically indexed by the type,
+    -- the one from 'Data.Typeable' doesn't.
     TypeMismatch SomeTypeRep SomeTypeRep
 
 apply :: forall b. (Typeable b) => DataFrame -> ColumnName -> (b -> b) -> Either ApplyError DataFrame
@@ -35,20 +47,24 @@ apply DataFrame {columns} columnName f =
     alteration :: Maybe SomeFrameList -> Either ApplyError (Maybe SomeFrameList)
     alteration = \case
       Nothing -> Left $ ColumnDoesNotExist columnName
-      -- The type annotation in the match binding the hidden type of tye elements into scope
+      -- The type annotation in the match binding brings the hidden type of the elements into scope.
       Just (MakeSomeFrameList {frameList = oldFrameList :: [a]}) ->
         -- We get the *indexed* type 'TypeRep's of the types we want to compare.
         -- It's not enough that we use the unindexed
         -- Type.Reflection.SomeTypeRep / Data.Typeable.TypeRep (same thing)
-        -- because the 'testEquality' function needs the indexed versions.
+        -- because the 'testEquality' function needs the indexed versions!
         let repb :: Type.Reflection.TypeRep b = Type.Reflection.typeRep @b
             repa :: Type.Reflection.TypeRep a = Type.Reflection.typeRep @a
          in case repa `testEquality` repb of
               Nothing ->
                 Left $
                   TypeMismatch (SomeTypeRep repb) (SomeTypeRep repa)
-              -- By the magic of matching on the evidence :~: that the types are equal,
-              -- GHC *knows* that 'a' and 'b' are the same type, while inside the pattern match.
+              -- By the GADT magic of matching on the evidence :~: that the types are equal,
+              -- while inside the pattern match, GHC *knows* that 'a' and 'b' are the same type,
+              -- which allows us to apply the function!
+              --
+              -- Outside of the 'Refl' pattern match, GHC considers the types to be
+              -- different and trying to apply the function would result in a type error.
               Just Refl ->
                 Right $
                   Just $
